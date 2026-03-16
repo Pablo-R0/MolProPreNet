feda7bd6ce3b5e917a71577eebdd67103ff01530
Pablo-R0 <pablo.roseiro@orange.fr>
2026-03-16 10:09:03 +0100

    Setting up smiles_to_graph from tools.py Small W.I.P notebook using RDKit_testing_polymers.ipynb 	new file:   RDKit_testing_polymers.ipynb 	new file:   tools.py

---
+++ tools.py
@@ -0,0 +1,23 @@
+from rdkit import Chem
+from torch_geometric.data import Data
+import torch
+
+def smiles_to_graph(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    # Create the edge_index [2, 2*num_edges]
+    edge_index = [[],[]]
+    for bond in mol.GetBonds():
+        beginatomidx, endatomidx = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        edge_index[0].extend([beginatomidx, endatomidx])
+        edge_index[1].extend([endatomidx, beginatomidx])
+    edge_index = torch.tensor(edge_index, dtype=torch.long)
+    # Create the nodes features [num_nodes, num_node_features]
+    nodes_features = []
+    for atom in mol.GetAtoms():
+        atom_features = [atom.GetAtomicNum(), atom.GetDegree(), atom.GetFormalCharge(), atom.IsInRing(), atom.GetIsAromatic(), int(atom.GetHybridization())]
+        nodes_features.append(atom_features)
+    nodes_features = torch.tensor(nodes_features, dtype=torch.long)
+
+    # Create the data object
+    data = Data(x=nodes_features, edge_index=edge_index)
+    return data
